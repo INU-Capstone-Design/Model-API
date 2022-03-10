0d9de20e88dd8f1024856331611a4b196f324c41
장성태 <wkdtjdxo1234@naver.com>
2022-03-11 00:23:47 +0900

    add ko_w2v_model.zip
    word2vec 모델 추가

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask                    # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource      # Api 구현을 위한 Api 객체 import
 from gensim.models import KeyedVectors
-from googletrans import Translator
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)         # Flask 객체에 Api 객체 등록
@@ -9,12 +8,10 @@ api = Api(app)         # Flask 객체에 Api 객체 등록
 @app.route('/<str:ko_word>')
 class Model(Resource):
     def get(self, ko_word: str):
-        global model
-        translator = Translator()
-        en_word = translator.translate(ko_word, src='ko', dest='en')
+        global model    # 모델 전역 변수 선언
 
         try: # 모델에 단어 입력
-            result = model.most_similar(en_word)
+            result = model.most_similar(ko_word)
             return {
                 'status': 1,
                 'result': result
@@ -26,5 +23,5 @@ class Model(Resource):
             }
 
 if __name__ == "__main__":
-    model = KeyedVectors.load_word2vec_format("모델 이름") # 모델 로드
+    model = KeyedVectors.load_word2vec_format("ko_w2v_model") # 모델 로드
     app.run(debug=True, port=80)
